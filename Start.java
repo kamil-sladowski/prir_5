159160c5669e105d5a6d723bf81e8b89dccccf09
kamilo116 <kamil.sladowski@gmail.com>
2018-12-12 22:32:56 +0100

    ddaad

--- Start.java
+++ Start.java
@@ -64,37 +64,59 @@ class OptimizationImpl extends optimizationPOA implements optimizationOperations
 
     @Override
     public void best_range(rangeHolder r) {
-        ArrayList<Short> tmpRange = new ArrayList<>();
-        ArrayList<Short> maxRange = new ArrayList<>();
-        for (SingleServer e: servers.values()){
-            if(e.isActive()){
-                tmpRange.add(Short.valueOf(e.ip));
+        range bestRange = null, tmpRange = null;
+        Iterator<SingleServer> it = (Iterator<SingleServer>) servers.values();
+        while (it.hasNext()) {
+            SingleServer sItem = it.next();
+            if (tmpRange == null && sItem.isActive()) {
+                tmpRange = new range(sItem.ip, sItem.ip);
+            } else if (tmpRange != null && sItem.isActive()) {
+                if (sItem.ip - 1 == tmpRange.to) {
+                    tmpRange.to += 1;
+                } else {
+                    tmpRange = new range(sItem.ip, sItem.ip);
+                }
+            } else {
+                tmpRange = null;
             }
-            else{
-
-                addressRange.add(tmpRange);
-                tmpRange = new ArrayList<>();
+            if (bestRange == null || tmpRange != null && tmpRange.to - tmpRange.from > bestRange.to - bestRange.from) {
+                bestRange = tmpRange;
             }
         }
-        int maxSize = 0;
+        r.value = bestRange;
+//        ArrayList<Short> tmpRange = new ArrayList<>();
+//        ArrayList<Short> maxRange = new ArrayList<>();
+//        for (SingleServer e: servers.values()){
+//            if(e.isActive()){
+//                tmpRange.add(Short.valueOf(e.ip));
+//            }
+//            else{
+//
+//                addressRange.add(tmpRange);
+//                tmpRange = new ArrayList<>();
+//            }
+//        }
+//        int maxSize = 0;
+//
+//        for(ArrayList<Short> oneList: addressRange) {
+//            if (maxSize < oneList.size()) {
+//                maxSize = oneList.size();
+//                maxRange = oneList;
+//            }
+//        }
+//        if (maxRange.size() != 0) {
+//
+//            Short[] stockArr = new Short[maxRange.size()];
+//            stockArr = maxRange.toArray(stockArr);
+//            for (Short el: stockArr)
+//                System.out.println("   -----    " + el);
+//            System.out.println("   -stockArr[0]    " + stockArr[0]);
+//            System.out.println("   -stockArr[stockArr.length-1]    " + stockArr[stockArr.length-1]);
+//            range a = new range(stockArr[0], stockArr[stockArr.length-1]);
+//            r.value = a;
+//        }
 
-        for(ArrayList<Short> oneList: addressRange) {
-            if (maxSize < oneList.size()) {
-                maxSize = oneList.size();
-                maxRange = oneList;
-            }
-        }
-        if (maxRange.size() != 0) {
 
-            Short[] stockArr = new Short[maxRange.size()];
-            stockArr = maxRange.toArray(stockArr);
-            for (Short el: stockArr)
-                System.out.println("   -----    " + el);
-            System.out.println("   -stockArr[0]    " + stockArr[0]);
-            System.out.println("   -stockArr[stockArr.length-1]    " + stockArr[stockArr.length-1]);
-            range a = new range(stockArr[0], stockArr[stockArr.length-1]);
-            r.value = a;
-        }
     }
 }
 
